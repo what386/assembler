25d236d3a7002af9013172eaf2bf043272393702
bmorin <robot0324@gmail.com>
2026-03-29 16:36:51 -0400

    upd

--- docs/example.asm
+++ docs/example.asm
@@ -1,7 +1,7 @@
 .section text
 .page 0                  ; label syntax, comment style shown for intent only
 start:
-    lim r0, !0x10
+    lim r0, 0x10
     mld r1, [0x0200]
     mlx r2, [r3+4]
     mst r1, [0x0201]
@@ -12,22 +12,22 @@ start:
 .page 1
 loop:
     add r2, r2, r3
-    addi r3, !1
-    cmpi r3, !11
+    addi r3, 1
+    cmpi r3, 11
     bra ?lower, loop
 
     mst r2, [0x0200]
     mld r4, [0x0200]
 
-    lim r5, !0x0300
+    lim r5, 0x0300
     msx r4, [r5+0]
     mlx r6, [r5+0]
 
-    tsti r6, !1
+    tsti r6, 1
     cmov r7, r6, ?not_equal
     bra ?equal, done
 
-    lim r7, !0
+    lim r7, 0
     jmp done
 
 .page 2
