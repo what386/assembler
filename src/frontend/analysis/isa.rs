a2206f6ed61c1e9d84cc7dc3f5767ba5c3f2d06f
bmorin <robot0324@gmail.com>
2026-03-29 20:34:19 -0400

    update instruction aliases

--- src/frontend/analysis/isa.rs
+++ src/frontend/analysis/isa.rs
@@ -1,5 +1,7 @@
+use phf::phf_map;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum OperandFormatKind {
+pub enum OpFormatKind {
     Register,
     Condition,
     Address,
@@ -12,13 +14,15 @@ pub enum OperandFormatKind {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct OperandFormat {
     pub operand_order: u8,
-    pub kind: OperandFormatKind,
+    pub kind: OpFormatKind,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Bitfield {
     Operand(OperandFormat),
+    // type of instruction. Set automatically by name table
     Kind(u8), // bit length
+    // adds raw data to the output instruction
     Pad{ data: i32, length: u8 },
 }
 
@@ -29,13 +33,13 @@ pub struct InstFmt {
     pub bitfields: &'static [Bitfield],
 }
 
-macro_rules! reg  { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OperandFormatKind::Register }) }; }
-macro_rules! cond { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OperandFormatKind::Condition }) }; }
-macro_rules! addr { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OperandFormatKind::Address }) }; }
-macro_rules! ptr  { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OperandFormatKind::Pointer }) }; }
-macro_rules! off  { ($order:expr, $len:expr) => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OperandFormatKind::Offset { bit_length: $len } }) }; }
-macro_rules! ptroff { ($order:expr)          => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OperandFormatKind::OffsetPointer }) }; }
-macro_rules! imm  { ($order:expr, $len:expr) => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OperandFormatKind::Immediate { bit_length: $len } }) }; }
+macro_rules! reg  { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OpFormatKind::Register }) }; }
+macro_rules! cond { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OpFormatKind::Condition }) }; }
+macro_rules! addr { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OpFormatKind::Address }) }; }
+macro_rules! ptr  { ($order:expr)            => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OpFormatKind::Pointer }) }; }
+macro_rules! off  { ($order:expr, $len:expr) => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OpFormatKind::Offset { bit_length: $len } }) }; }
+macro_rules! ptroff { ($order:expr)          => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OpFormatKind::OffsetPointer }) }; }
+macro_rules! imm  { ($order:expr, $len:expr) => { Bitfield::Operand(OperandFormat { operand_order: $order, kind: OpFormatKind::Immediate { bit_length: $len } }) }; }
 
 macro_rules! kind { ($len:expr) => { Bitfield::Kind($len) }; }
 macro_rules! pad { ($data:expr, $len:expr) => { Bitfield::Pad{data: $data, length: $len} }; }
@@ -90,3 +94,62 @@ pub const PSEUDO_INSTRUCTION_SET: &[InstFmt] = &[
     inst!("10010", "inc", [reg!(0), pad!(1,8)]),                       // increment -> add immediate
     inst!("10010", "dec", [reg!(0), pad!(-1,8)]),                      // decrement -> add immediate
 ];
+
+pub static INSTRUCTION_ALIASES: phf::Map<&'static str, (&'static str, u8)> = phf_map! {
+    "ret"   => ("crets", 0b00),
+    "brk"   => ("crets", 0b01),
+    "iret"  => ("crets", 0b11),
+
+    "pop"   => ("pop",  0b00),
+    "peek"  => ("pop",  0b01),
+    "popf"  => ("pop",  0b10),
+    "dsp"   => ("pop",  0b11),
+
+    "psh"   => ("psh",  0b00),
+    "poke"  => ("psh",  0b01),
+    "pshf"  => ("psh",  0b10),
+    "isp"   => ("psh",  0b11),
+
+    "mov"   => ("cmov", 0b00),
+    "xchg"  => ("cmov", 0b01),
+
+    "add"   => ("add",  0b00),
+    "adc"   => ("add",  0b01),
+    "adv"   => ("add",  0b10),
+    "advc"  => ("add",  0b11),
+
+    "sub"   => ("sub",  0b00),
+    "sbb"   => ("sub",  0b01),
+    "sbv"   => ("sub",  0b10),
+    "sbvb"  => ("sub",  0b11),
+
+    "and"   => ("bitw", 0b00),
+    "or"    => ("bitw", 0b01),
+    "xor"   => ("bitw", 0b10),
+    "imp"   => ("bitw", 0b11),
+
+    "nand"  => ("bntw", 0b00),
+    "nor"   => ("bntw", 0b01),
+    "xnor"  => ("bntw", 0b10),
+    "nimp"  => ("bntw", 0b11),
+
+    "bsl"   => ("bsh",  0b00),
+    "bsr"   => ("bsh",  0b01),
+    "rol"   => ("bsh",  0b10),
+    "bsxr"  => ("bsh",  0b11),
+
+    "bsli"  => ("bshi", 0b00),
+    "bsri"  => ("bshi", 0b01),
+    "roli"  => ("bshi", 0b10),
+    "bsxri" => ("bshi", 0b11),
+
+    "mul"   => ("mdo",  0b00),
+    "mulu"  => ("mdo",  0b01),
+    "div"   => ("mdo",  0b10),
+    "mod"   => ("mdo",  0b11),
+
+    "sqrt"  => ("btc",  0b00),
+    "clz"   => ("btc",  0b01),
+    "ctz"   => ("btc",  0b10),
+    "popcnt"=> ("btc",  0b11),
+};
